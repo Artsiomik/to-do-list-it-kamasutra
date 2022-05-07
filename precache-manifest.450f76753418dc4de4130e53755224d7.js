339d4ba78fc2d20e0e625c4acf1a525e86294626
Artsiom <Artyom.artyom.87@bk.ru>
2022-05-07 09:59:10 +0300

    Updates

---
+++ precache-manifest.450f76753418dc4de4130e53755224d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a349ec8856aa178034971caec552d4a",
+    "url": "/to-do-list-it-kamasutra/index.html"
+  },
+  {
+    "revision": "495b385c78d3b1d944a3",
+    "url": "/to-do-list-it-kamasutra/static/css/main.48e9433a.chunk.css"
+  },
+  {
+    "revision": "3649be2dc8b8e3193387",
+    "url": "/to-do-list-it-kamasutra/static/js/2.d99582fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/to-do-list-it-kamasutra/static/js/2.d99582fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495b385c78d3b1d944a3",
+    "url": "/to-do-list-it-kamasutra/static/js/main.c465c3f5.chunk.js"
+  },
+  {
+    "revision": "2512520b3f2083ad1fcb",
+    "url": "/to-do-list-it-kamasutra/static/js/runtime-main.93c42b6c.js"
+  }
+]);
